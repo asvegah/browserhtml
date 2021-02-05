e5dd14e59b6c2b808ac32a0cdcf673f38c45f835
Ahmad Vegah <ahmadvegah@gmail.com>
2021-02-05 14:56:31 +0000

    first commit

---
+++ min/js/navbar/menuButton.js
@@ -0,0 +1,27 @@
+var keybindings = require('keybindings.js')
+var settings = require('util/settings/settings.js')
+
+var menuButton = document.getElementById('menu-button')
+
+function showSecondaryMenu () {
+  var navbar = document.getElementById('navbar')
+  var rect = menuButton.getBoundingClientRect()
+  var navbarRect = navbar.getBoundingClientRect()
+
+  ipc.send('showSecondaryMenu', {
+    x: Math.round(rect.left),
+    y: Math.round(navbarRect.bottom)
+  })
+}
+
+function initialize () {
+  menuButton.addEventListener('click', function (e) {
+    showSecondaryMenu()
+  })
+
+  keybindings.defineShortcut('showMenu', function () {
+    if (!settings.get('useSeparateTitlebar') && (window.platformType === 'windows' || window.platformType === 'linux')) { showSecondaryMenu() }
+  })
+}
+
+module.exports = { initialize, showSecondaryMenu }
